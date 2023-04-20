1ac23d38ee315eb919d811ff1ccde9497ae283fb
Sean Craft <sean.craft@nrlssc.navy.mil>
2023-04-20 17:02:49 -0500

    legacy

--- src/main/groovy/nrlssc/gradle/NRLPlugin.groovy
+++ src/main/groovy/nrlssc/gradle/NRLPlugin.groovy
@@ -107,9 +107,9 @@ class NRLPlugin implements Plugin<Project>{
                     }
                     maven {
                         if (nrl.remoteAllowed()) {
-                            url "$nrl.artiURL/${RepoNames.RemoteMavenRepo.getName(nrl.groupCode)}"
+                            url "$nrl.artiLegacyURL/${RepoNames.RemoteMavenRepo.getName(nrl.groupCode)}"
                         } else {
-                            url "$nrl.artiURL/${RepoNames.LocalMavenRepo.getName(nrl.groupCode)}"
+                            url "$nrl.artiLegacyURL/${RepoNames.LocalMavenRepo.getName(nrl.groupCode)}"
                         }
                         allowInsecureProtocol = true
 
